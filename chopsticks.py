83259bfc94dd31a0bf8443d4bcb1477745983e51
jackstrosahl <strosahljack@gmail.com>
2022-11-22 17:36:50 -0500

    Cleaned up with visualization

--- chopsticks.py
+++ chopsticks.py
@@ -1,5 +1,10 @@
-from collections import OrderedDict, defaultdict
+from networkx import DiGraph, draw
+import matplotlib.pyplot as plt
+from networkx.drawing.nx_pydot import graphviz_layout
 import queue
+
+def pos_string(position):
+    return "".join("".join(str(hand) for hand in player) for player in position)
 
 # Only 2 for now
 num_hands = 2
@@ -7,29 +12,39 @@ num_players = 2
 rollover = 5
 start_hand = (1,1)
 
-state = (0,*(start_hand for _ in range(num_players)))
+state = (0,tuple(start_hand for _ in range(num_players)),None)
 q = queue.Queue()
 explored = set()
 q.put(state)
-position_set = set()
 
+graph = DiGraph()
 while not q.empty():
     state = q.get()
-    move, *players = state
-    turn = move % num_players
-    position = (turn, *players)
-    alt_position = tuple(players if turn == 0 else reversed(players))
-    if alt_position in explored:
+    move, position, parent = state
+    if parent is not None:
+        graph.add_edge(pos_string(parent),pos_string(position))
+    if position in explored:
         continue
+    explored.add(position)
+    
 
-    cur_player = players[turn]
-    next_move = move + 1
-    # Generate all transfers and divisions
+    cur_player = position[0]
+    next_move = move + 1        
+    
+    def queue_move(new_player,i=0):
+        new_position = list(position[1:])
+        if i == 0:
+            new_position.append(new_player)
+        else:
+            new_position.append(position[0])
+            new_position[i-1] = new_player
+        q.put((next_move, tuple(new_position), position))
+
     sum_fingers = sum(cur_player)
 
-    position_set.add(alt_position)
-    explored.add(alt_position)
+    
 
+    # Generate all transfers and divisions
     if sum_fingers == 0:
         continue
 
@@ -39,17 +54,14 @@ while not q.empty():
         o_fingers = sum_fingers - fingers
         if o_fingers >= rollover:
             continue
-        new_players = players[:]
         new_player = tuple(sorted((fingers, o_fingers)))
         if new_player == cur_player or (min_hand > 0 and min(new_player) == 0):
             continue
-        new_players[turn] = new_player
-        q.put((next_move,*new_players))
+        queue_move(new_player)
+
     # Generate all attacks
-    for opp_i in range(num_players):
-        if opp_i == turn:
-            continue
-        opp_player = players[opp_i]
+    for opp_i in range(1,num_players):
+        opp_player = position[opp_i]
         for fingers in cur_player:
             if fingers == 0:
                 continue
@@ -58,8 +70,9 @@ while not q.empty():
                 if new_player[opp_hand] == 0:
                     continue
                 new_player[opp_hand] = (new_player[opp_hand] + fingers) % rollover
-                new_players = players[:]
-                new_players[opp_i] = tuple(sorted(new_player))
-                q.put((next_move,*new_players))
+                queue_move(tuple(sorted(new_player)),opp_i)
 
 print(len(explored))
+pos = graphviz_layout(graph, prog="dot")
+draw(graph, pos, with_labels=True, node_size=800)
+plt.show()
